a61a50dff5ad351ed811d70c80ca89eb1eacc3eb
paucabral <josepaucabral@gmail.com>
2021-11-23 14:47:28 +0800

    Created method for interacting with the registration using API.

--- api.py
+++ api.py
@@ -10,7 +10,25 @@ ENDPOINT = "http://localhost:5000"
 def login(username, password):
     route = "api/login"
     query = "{}/{}".format(ENDPOINT, route)
-    body = {"username": username, "password": password}
+    body = {
+        "username": username,
+        "password": password
+    }
+    response = post(query, json=body)
+
+    return response.json()
+
+
+def register(username, password, email, first_name, last_name):
+    route = "api/register"
+    query = "{}/{}".format(ENDPOINT, route)
+    body = {
+        "username": username,
+        "password": password,
+        "email": email,
+        "first_name": first_name,
+        "last_name": last_name
+    }
     response = post(query, json=body)
 
     return response.json()
